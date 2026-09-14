6550015b41979ab81464b8b630252cf4e43db846
kunal bhanuse <kunalbhanuse07@gmail.com>
2026-09-14 08:01:13 +0530

    feat: implement embeddings functionality using OpenAI API

---
+++ 01-transformers/embeddings.js
@@ -0,0 +1,14 @@
+import "dotenv/config";
+import OpenAI from "openai";
+
+const cleint = new OpenAI();
+
+const embedding = await cleint.embeddings.create({
+  model: "text-embedding-3-small",
+  input: "Your text string goes here",
+  encoding_format: "float",
+});
+
+console.log(embedding.data[0].embedding);
+
+// so function to get the embeddings
