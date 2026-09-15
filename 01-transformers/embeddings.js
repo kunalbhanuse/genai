b43ea615af63b5dcf28a89b920d11033096a81aa
kunal bhanuse <kunalbhanuse07@gmail.com>
2026-09-15 08:18:03 +0530

    fix: update input string for embeddings example

--- 01-transformers/embeddings.js
+++ 01-transformers/embeddings.js
@@ -5,7 +5,7 @@ const cleint = new OpenAI();
 
 const embedding = await cleint.embeddings.create({
   model: "text-embedding-3-small",
-  input: "Your text string goes here",
+  input: "Hi this is the embeddings ",
   encoding_format: "float",
 });
 
