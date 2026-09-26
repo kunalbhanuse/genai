134d2f428441b531b2c9fd34deb297be4e3dc548
kunal bhanuse <kunalbhanuse07@gmail.com>
2026-09-26 22:06:29 +0530

    fix: correct input string for embeddings

--- 01-transformers/embeddings.js
+++ 01-transformers/embeddings.js
@@ -5,10 +5,10 @@ const cleint = new OpenAI();
 
 const embedding = await cleint.embeddings.create({
   model: "text-embedding-3-small",
-  input: "Hi kunal bhanuse  .",
+  input: "Hi kunal bhanuse a sowtware enginerr .",
   encoding_format: "float",
 });
 
-console.log(embedding.data[0].embedding);
+console.log(embedding.data[0].embedding.length);
 
 // so function to get the embeddings
